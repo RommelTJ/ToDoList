5d92766ec6a1818a45010c5c8fb1f121d2c1f629
Rommel Rico <rommeltj@gmail.com>
2015-02-26 20:57:03 -0800

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Udemy Tutorial 6
+
+Version: 0.0.1 - 26 Feb 2015
+
+## Description
+
+Udemy iOS/Swift tutorials.
+
+### Documentation
+Branches:  
+master = Production branch  
+dev = Development branch  
+
+## Contact
+
+<me@rommelrico.com>
+
+## Notes
+
+If you want to contribute, email me at <me@rommelrico.com>.
---
+++ ToDoList/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ToDoList
+//
+//  Created by Rommel Rico on 2/26/15.
+//  Copyright (c) 2015 Rommel Rico. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
